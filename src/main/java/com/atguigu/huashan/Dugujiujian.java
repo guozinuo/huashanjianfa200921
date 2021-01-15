43037afdfe2d318d561b1c2dadf27103e97fd452
duguqiubai <961460411@qq.com>
2021-01-15 22:28:12 +0800

    增加独孤九剑

---
+++ src/main/java/com/atguigu/huashan/Dugujiujian.java
@@ -0,0 +1,12 @@
+package com.atguigu.huashan;
+
+public class Dugujiujian {
+	public static void main(String[] args) {
+		System.out.println("独孤九剑奥力给！");
+		System.out.println("独孤九剑奥力给！");
+		System.out.println("独孤九剑奥力给！");
+		System.out.println("独孤九剑奥力给！");
+		System.out.println("独孤九剑奥力给！");
+		System.out.println("独孤九剑奥力给！");
+	}
+}
