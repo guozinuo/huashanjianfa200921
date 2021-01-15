023625f56ab8811b0ded27d9e66f76d97c355c05
duguqiubai <961460411@qq.com>
2021-01-15 22:56:21 +0800

    增加独孤九剑修改

--- src/main/java/com/atguigu/huashan/Dugujiujian.java
+++ src/main/java/com/atguigu/huashan/Dugujiujian.java
@@ -9,5 +9,10 @@ public class Dugujiujian {
 		System.out.println("独孤九剑奥力给！");
 		System.out.println("独孤九剑奥力给！");
 		System.out.println("修改");
+		System.out.println("剑宗好");
+		System.out.println("剑宗好");
+		System.out.println("剑宗好");
+		System.out.println("剑宗好");
+		System.out.println("剑宗好");
 	}
 }
